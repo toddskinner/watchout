fecc2277fb37c23cf8d5ad211daec7efa4bcbbf0
toddskinner <todd.skinner@gmail.com>
2014-12-19 15:10:59 -0800

    Add enemy

--- watchout.js
+++ watchout.js
@@ -1,2 +1,23 @@
 // start slingin' some d3 here.
 
+
+
+var width = 960,
+    height = 500,
+    border = "solid",
+    bgcolor = "black";
+
+
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("border", border)
+    .style("background-color", bgcolor);
+
+var enemy = d3.select("svg").append("image")
+  .attr("width", 50)
+  .attr("height", 50)
+  .attr("x", 480)
+  .attr("y", 250)
+  .attr("xlink:href", "asteroid.png");
