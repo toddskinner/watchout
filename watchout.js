a8f6ed20fa2e47ff09a620fb0b1b6a7b86f5e31d
toddskinner <todd.skinner@gmail.com>
2014-12-20 16:52:01 -0800

    Add new features

--- watchout.js
+++ watchout.js
@@ -26,26 +26,30 @@ var svg = d3.select("body").append("svg")
 var createEnemy = function(){
   for (var i = 0; i < n; i++) {
     var enemy = d3.select("svg").append("image")
+    .attr("class", "shuriken")
     .attr("width", enemyWidth)
     .attr("height", enemyHeight)
     .attr("x", Math.floor(Math.random()*width))
     .attr("y", Math.floor(Math.random()*height))
-    .attr("xlink:href", "shuriken.png");
+    .attr("xlink:href", "alien.png");
   }
 };
 
 var createPlayer = function(){
   var drag = d3.behavior.drag()
              .on('drag', function() {
-              playerCoordinates.x = d3.event.x;
-              playerCoordinates.y = d3.event.y;
-              player.attr('cx', d3.event.x)
-              .attr('cy', d3.event.y);
+              if(d3.event.x > 0 && d3.event.x < 960
+                && d3.event.y < 500 && d3.event.y > 0){
+                playerCoordinates.x = d3.event.x;
+                playerCoordinates.y = d3.event.y;
+                player.attr('cx', d3.event.x)
+                .attr('cy', d3.event.y);
+              }
             });
 
   var player = d3.select("svg").append("circle")
-  .attr("cx", width / 2)
-  .attr("cy", height / 2)
+  .attr("cx", playerCoordinates.x)
+  .attr("cy", playerCoordinates.y)
   .attr("r", playerRadius)
   .attr("fill", "yellow")
   .call(drag);
@@ -99,11 +103,30 @@ var collision = function(){
     var yDiff = playerCoordinates.y - parseFloat(domElements[0][i].attributes.y.value);
     var separation = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
     if(separation < radiusSum){
+      d3.select("svg")
+      .style("background", "url(" + "boom.jpg" + ") no-repeat")
+      .style("background-size", 100 + "%");
+      // d3.select("svg").append("image")
+      // .attr("class", "fireball")
+      // .attr("width", enemyWidth)
+      // .attr("height", enemyHeight)
+      // .transition().duration(200)
+      // .attr("x", parseFloat(domElements[0][i].attributes.x.value))
+      // .attr("y", parseFloat(domElements[0][i].attributes.y.value))
+      // .attr("xlink:href", "fireball.png");
       scoreCollision();
     }
   }
   updateScore();
 };
+
+ var changeBackground = function(){
+  d3.select("svg")
+      .style("background", "url(" + "space-lols.jpg" + ") no-repeat")
+      .style("background-size", 100 + "%");
+};
+
+setInterval(changeBackground, 500);
 
 var scoreCollision = function(){
   scores[2]++;
